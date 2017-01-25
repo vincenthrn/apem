35920040e1d193b100ace693881d6fc78c2cba13
Vincent <vincent.henrion@hetic.net>
2017-01-25 16:54:05 +0100

    motif

--- wp-content/themes/angle/functions.php
+++ wp-content/themes/angle/functions.php
@@ -24,6 +24,16 @@ include OXY_THEME_DIR . 'inc/woocommerce.php';
 include OXY_THEME_DIR . 'vendor/oxygenna/oxygenna-framework/inc/OxygennaTheme.php';
 include OXY_THEME_DIR . 'vendor/oxygenna/oxygenna-mega-menu/oxygenna-mega-menu.php';
 
+
+
+if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+function my_jquery_enqueue() {
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js", false, null);
+    wp_enqueue_script('jquery');
+}
+
+
 // create theme
 
 global $oxy_theme;
